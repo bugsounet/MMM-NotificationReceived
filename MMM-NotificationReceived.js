d3c6fc5c481c8b6a7f67812a9258a377d24c0320
bugsounet <bugsounet@bugsnana.fr>
2020-05-13 16:11:14 +0200

    first

---
+++ MMM-NotificationReceived.js
@@ -0,0 +1,22 @@
+/** MMM-NotificationReceived **/
+
+Module.register("MMM-NotificationReceived", {
+
+  defaults: {
+    notificationFrom : [ "clock", "calendar" ]
+  },
+
+  start: function () {
+    console.log("[Notification] Started...")
+  },
+
+  notificationReceived: function (notification, payload, sender) {
+    let displayed = false
+    if (!sender) return
+    this.config.notificationFrom.forEach(notiSender => {
+      if (sender.name == notiSender) return displayed = true 
+    })
+    if (displayed) console.log("[Notification][" + sender.name + "] " + notification, payload ? payload : "")
+  },
+
+});
